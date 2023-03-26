61a7c5e9e990fa782b3e6b0d16e01fa9c9461425
manas20443 <89641459+manas20443@users.noreply.github.com>
2023-03-26 18:06:30 +0530

    Tweet Generation Python Code

--- DatasetGeneration/generateTweets.py
+++ DatasetGeneration/generateTweets.py
@@ -1,1 +1,19 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
 
+query = "(from:ReallySwara) until:2023-03-24 since:2022-10-01"
+tweets = []
+limit = 2000
+
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+
+    # print(vars(tweet))
+    # break
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date, tweet.username, tweet.content])
+
+df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+print(df)
+print(df.head())
